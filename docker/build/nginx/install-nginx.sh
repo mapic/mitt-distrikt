4d634f74b64e30fa4ca5eda7e3e67d7d3089e17f
knutole <knutole@mapic.io>
2017-04-21 23:38:37 +0200

    reconfigured builds

--- docker/build/nginx/install-nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo deb http://nginx.org/packages/ubuntu/ xenial nginx >> /etc/apt/sources.list.d/nginx.list
-echo deb-src http://nginx.org/packages/ubuntu/ xenial nginx >> /etc/apt/sources.list.d/nginx.list
-apt-get update -y
-apt-get install nginx -y --allow-unauthenticated

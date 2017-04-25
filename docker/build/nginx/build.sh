6f4cb8118272897612809a988039a7e69cb59c68
knutole <knutole@mapic.io>
2017-04-25 09:57:18 +0200

    custom build of nginx

---
+++ docker/build/nginx/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create crypto
+if test -f dhparams.pem; then
+  echo 'Using pre-existing Strong Diffie-Hellmann Group'
+else
+  echo 'Creating Strong Diffie-Hellmann Group'
+  openssl dhparam -out dhparams.pem 2048 || exit 1
+fi
+
+# build
+echo 'Building image'
+docker build -t lier/nginx:latest . || exit 1

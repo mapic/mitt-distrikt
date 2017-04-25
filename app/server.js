881a67c5f6aa7f22a65723553a9e27200b48e29b
knutole <knutole@mapic.io>
2017-04-25 10:19:55 +0200

    included cookies, hardening in server

--- app/server.js
+++ app/server.js
@@ -1,15 +1,33 @@
 // webserver
 
+// load modules
 var express = require('express');
+var helmet = require('helmet')
+var cookieParser = require('cookie-parser')
+
+// create app
 var app = express();
 
+// use cookies
+// https://github.com/expressjs/cookie-parser#example
+app.use(cookieParser())
+
+// harden security
+app.use(helmet());
+app.disable('x-powered-by')
+
+// set port
 var port = 3001;
+
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
+    console.log('Cookies: ', req.cookies);
+    console.log('Signed Cookies: ', req.signedCookies)
+    res.send('hello world')
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port', port)
-})
+    console.log('Example app listening on port', port)
+});
+

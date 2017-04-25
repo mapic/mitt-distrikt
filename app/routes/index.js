75d66f05ca8fac9ef25dcbd6b9e1a31f578295f8
knutole <knutole@mapic.io>
2017-04-25 11:57:00 +0200

    added app framework with routes and views

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    console.log('Cookies: ', req.cookies);
+    console.log('Signed Cookies: ', req.signedCookies)
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-# install deps: nodejs, docker, docker compose, etc.
+# install nodejs
+
+# install docker
+
+# install docker-compose
+
+# install htop, fish, wget, curl, etc.
+
+# install node packages
+sudo npm i nsp -g
+sudo npm install -g snyk
+
 
 # build all images
 

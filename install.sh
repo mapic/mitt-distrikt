d655e7783c257bd45e89d026eac122e1f39bfdf4
knutole <knutole@mapic.io>
2017-04-26 11:35:34 +0000

    Added comments to install script

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# install deps
+installing () {
+    clear 
+    echo "Installing $1..."
+}
+
+# update/upgrade
+installing "updates"
 apt-get update -y
 apt-get upgrade -y
 apt-get update -y
+
+# install tools/deps
+installing "tools and dependencies"
 apt-get install -y nmap fish htop wget curl git python-pip build-essential apt-transport-https ca-certificates curl software-properties-common
 
 # install docker
+installing "Docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -14,23 +24,26 @@ sudo apt-get update -y
 sudo apt-get install -y docker-ce 
 
 # install docker-compose
+installing "Docker Compose"
 pip install docker-compose
 
 # install nodejs
+installing "NodeJS"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # install node packages
+installing "security packages"
 sudo npm i nsp -g
 sudo npm install -g snyk
 
 # install certbot
+installing "Certbot for SSL certificates"
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update -y
 sudo apt-get install -y certbot 
 
 # build all images
+installing "images"
 cd docker/build
 sh build-all.sh
-# build it!
-#

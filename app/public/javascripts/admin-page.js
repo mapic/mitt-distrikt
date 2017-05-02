9a1977c535bebc2bd1d0e822ede6671d11caccc9
knutole <knutole@mapic.io>
2017-05-02 19:42:50 +0000

    Added admin INFO

--- app/public/javascripts/admin-page.js
+++ app/public/javascripts/admin-page.js
@@ -1,1 +1,170 @@
-console.log('admin!');
+console.log('admin-page.js');
+
+// main app
+L.Admin = L.Class.extend({
+
+    _buttons : {
+        info : {},
+        map : {},
+        media : {}
+    },
+
+    _content : {},
+
+    initialize : function (options) {
+
+        // shortcut
+        window.app = this;
+
+        // set options
+        L.setOptions(this, options);
+
+        // login first
+        this._initLogin();
+
+    },
+
+    _initLogin : function () {
+
+    },
+
+    _loggedIn : function () {
+
+        // get browser
+        this._detectDevice();
+
+        // set locale
+        this.locale = window.locale[options.locale || 'NO'];
+
+        // get html
+        this._initContent();
+
+        // set events
+        this._setEvents();
+
+        // fill content
+        this._fillContent();
+
+        // set default pane
+        this._show('info');
+
+    },
+
+    _initContent : function () {
+
+        // get container
+        this._container = L.DomUtil.get('app-container');
+        
+        // get content
+        this._content.info = L.DomUtil.get('content-info');
+        this._content.map = L.DomUtil.get('content-kart');
+        this._content.media = L.DomUtil.get('content-media');
+
+        // get buttons
+        this._buttons.info.div  = L.DomUtil.get('button-info');
+        this._buttons.map.div  = L.DomUtil.get('button-kart');
+        this._buttons.media.div = L.DomUtil.get('button-media');
+
+        // set button text
+        this._buttons.info.div.innerHTML = this.locale.buttons.info;
+        this._buttons.map.div.innerHTML = this.locale.buttons.map;
+        this._buttons.media.div.innerHTML = this.locale.buttons.media;
+
+        // set logo
+        if (this.isDesktop()) {
+            this._logo = L.DomUtil.get('site-logo');
+            this._logo.style.display = 'block';
+        }
+    },
+
+    _setEvents : function () {
+
+        // create hammer elements
+        var hammerOptions = {};
+        this._buttons.info.event = new Hammer(this._buttons.info.div, hammerOptions);
+        this._buttons.map.event = new Hammer(this._buttons.map.div, hammerOptions);
+        this._buttons.media.event = new Hammer(this._buttons.media.div, hammerOptions);
+        
+        // set events
+        this._buttons.info.event.on('tap', this._showInfo.bind(this));
+        this._buttons.map.event.on('tap', this._showMap.bind(this));
+        this._buttons.media.event.on('tap', this._showMedia.bind(this));
+    },
+
+    _detectDevice : function  () {
+        // see https://hgoebl.github.io/mobile-detect.js/doc/MobileDetect.html
+        this._md = new MobileDetect(window.navigator.userAgent);
+    },
+
+    isJustMobile : function () {
+        return this._md.mobile() && !this._md.tablet();
+    },
+
+    isMobile : function () {
+        return this._md.mobile();
+    },
+
+    isTablet : function () {
+        return this._md.tablet();
+    },
+
+    isDesktop : function () {
+        var isDesktop = !this._md.tablet() && !this._md.mobile();
+        return isDesktop;
+    },
+
+    // create tab content
+    _fillContent : function () {
+
+        var infoContent = L.DomUtil.create('div', 'admin-info-text', this._content.info);
+
+        var loginText = this.locale.admin.info.loginLinkText;
+        var text = this.locale.admin.info.loginText;
+        infoContent.innerHTML = '<a target="_blank" href="https://blog.mittlier.no/wp-admin/">' + loginText + '</a>' + ' ' + text;
+
+        // // info tab
+        // this.info = new L.Info({
+        //     container : this._content.info
+        // });
+
+        // // map tab
+        // this.map = new L.MapContent({
+        //     container : this._content.map
+        // });
+
+        // // media tab
+        // this.media = new L.Media({
+        //     container : this._content.media
+        // });
+    },
+
+    // helper fn to show/hide the three tabs
+    _showInfo  : function () { this._show('info');},
+    _showMap   : function () { this._show('map');},
+    _showMedia : function () { this._show('media');},
+    _show : function (page) {
+
+        // hide all
+        if (page != 'info') this._content.info.style.display = 'none';
+        if (page != 'map') this._content.map.style.display = 'none';
+        if (page != 'media') this._content.media.style.display = 'none';
+
+        // show/hide footer (should only show on info)
+        // this._footer.style.display = (page == 'info') ? 'block' : 'none';
+
+        // show selected
+        this._content[page].style.display = 'block';
+
+        // highlight button
+        this._highlightButton(page);
+
+    },
+
+    // highlight selected button
+    _highlightButton : function (highlighted) {
+        for (var btn in this._buttons) {
+            L.DomUtil.removeClass(this._buttons[btn].div, 'highlighted')
+        };
+        L.DomUtil.addClass(this._buttons[highlighted].div, 'highlighted');
+    },
+});
--- app/public/javascripts/locale.js
+++ app/public/javascripts/locale.js
@@ -7,10 +7,18 @@ locale.NO = {
         map : 'Kart',
         media : 'Media'
     },
+    
     footer : {
 
         login : 'Logg inn',
         text : '© Lier kommune 2017. ',
+    },
+
+    admin : {
+        info : {
+            loginLinkText : 'Logg inn',
+            loginText : 'til Wordpress for å redigere bloggposter.'
+        }
     }
 
 }

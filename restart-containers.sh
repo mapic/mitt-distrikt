310ed1a2811c5a6c597ce59ea8d3cb72ef930c18
knutole <knutole@mapic.io>
2017-05-11 17:02:18 +0000

    fixed logs in start/stop script

--- restart-containers.sh
+++ restart-containers.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # stop, remove old
-docker rm -f docker_nginx_1
-docker rm -f docker_wordpress_1
-docker rm -f docker_web_1
-docker rm -f docker_mysql_1
-docker rm -f docker_redis_1
+./stop-containers.sh
 
 # start compose
 cd docker
-docker-compose -f app.yml up
+docker-compose -f app.yml up -d 
+
+# show logs
+docker-compose -f app.yml logs -f
---
+++ stop-containers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# stop, remove old
+docker rm -f docker_nginx_1
+docker rm -f docker_wordpress_1
+docker rm -f docker_web_1
+docker rm -f docker_mysql_1
+docker rm -f docker_redis_1

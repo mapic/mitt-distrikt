16814078718ffa711eabb043d7a3544bb71ba817
knutole <knutole@mapic.io>
2017-05-15 17:45:19 +0000

    Added AUTH to redis - todo: create fresh pass on every restart

--- config/app-config.js
+++ config/app-config.js
@@ -2,11 +2,13 @@ module.exports = {
 
     // title
     portal_title : 'Mitt Lier',
-
+    domain : 'mittlier.no',
+    wordpress_domain : 'blog.mittlier.no',
 
     redis : {
         // key for geojson
-        geojson : 'mittlier-geojson'
+        geojson : 'mittlier-geojson',
+        auth : 'mmdNargzcoXMctLwdzdL'
     },
 
 }

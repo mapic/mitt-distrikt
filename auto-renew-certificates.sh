0fe7f21bf4298e843e46a2765d5acd651cbf623b
knutole <knutole@mapic.io>
2017-10-22 20:16:55 +0000

    cronjob compatible

--- auto-renew-certificates.sh
+++ auto-renew-certificates.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 DOMAIN=mittlier.no
+MDPATH=/mapic/mitt-distrikt
 
 clear
 echo ""
 echo "Renewing certificates for domains"
 echo ""
 
-bash stop-containers.sh
-cd install
+# stop app
+docker rm -f docker_nginx_1
+docker rm -f docker_wordpress_1
+docker rm -f docker_web_1
+docker rm -f docker_mysql_1
+docker rm -f docker_redis_1
+
+# create ssl
+cd $MDPATH/install
 bash create-ssl.sh $DOMAIN blog.$DOMAIN hello@mapic.io
-cd ..
-bash restart-containers.sh
+
+# start app
+cd $MDPATH/docker
+docker-compose -f app.yml up -d
